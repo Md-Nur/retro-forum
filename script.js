a63f739d090d478a98e1c77da2c58694b10e9512
Md-Nur <md.nurealamsiddiquee2004@gmail.com>
2024-03-05 18:22:26 +0600

    Just footer left and search functionallity

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const readingHistoryContainer = document.getElementById(
   "readingHistoryContainer"
 );
 const readingCount = document.getElementById("readingCount");
-let countOfReading = 0;
+const latestPostContainer = document.getElementById("latestPostContainer");
 
 const allPosts = async () => {
+  let countOfReading = 0;
   const url = "https://openapi.programming-hero.com/api/retro-forum/posts";
   const response = await fetch(url);
   const data = await response.json();
@@ -16,16 +17,20 @@ const allPosts = async () => {
       "flex rounded-xl shadow-xl p-5 bg-gray-100 hover:bg-blue-50 w-full gap-5 m-5";
 
     postContainer.innerHTML = `
+  <div class="indicator">
+    <span class="indicator-item badge badge-${
+      post.isActive ? "success" : "error"
+    }"></span> 
     <div class="avatar">
-    <div class="w-16 h-16 rounded-xl">
-      <img
-        src=${post.image}
-      />
+      <div class="w-16 h-16 rounded-xl">
+        <img src=${post.image}/>
+      </div>
     </div>
   </div>
+
   <div class="text-justify space-y-3 w-full">
     <p class="text-sm">
-      #${post.category} &MediumSpace;<span>Author: Awlad Hossen</span>
+      #${post.category} &MediumSpace;Author: ${post.author.name}
     </p>
     <h3 class="font-bold text-justify">${post.title}</h3>
     <p class="text-sm text-gray-700">
@@ -76,3 +81,55 @@ const allPosts = async () => {
 };
 
 allPosts();
+
+const latestPost = async () => {
+  const url =
+    "https://openapi.programming-hero.com/api/retro-forum/latest-posts";
+  const response = await fetch(url);
+  const data = await response.json();
+  data.forEach((post) => {
+    const postContainer = document.createElement("div");
+    postContainer.classList = "card max-w-96 bg-base-100 shadow-xl m-2";
+    postContainer.innerHTML = `
+      <figure class="p-5">
+        <img
+          src=${post.cover_image}
+          alt=${post.title}
+          class="rounded-xl"
+        />
+      </figure>
+      <div class="card-body -mt-7">
+        <p class="flex text-gray-700 text-sm items-start gap-2">
+          <img src="icons/date.svg" />
+          ${
+            post.author.posted_date
+              ? post.author.posted_date
+              : "No publish date"
+          }
+        </p>
+        <h2 class="card-title">${post.title}</h2>
+        <p class="text-gray-700">
+         ${post.description}
+        </p>
+        <div class="flex gap-2 items-center">
+          <figure class="rounded-full w-10 h-10">
+            <img
+              src=${post.profile_image}
+            />
+          </figure>
+          <div>
+            <p class="font-semibold">${
+              post.author.name ? post.author.name : "Unknown"
+            }</p>
+            <p class="text-gray-600">${
+              post.author.designation ? post.author.designation : "Unknown"
+            }</p>
+          </div>
+        </div>
+      </div>
+  `;
+    latestPostContainer.appendChild(postContainer);
+  });
+};
+
+latestPost();
